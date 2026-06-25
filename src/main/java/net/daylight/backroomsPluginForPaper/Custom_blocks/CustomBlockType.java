04ba3ebc076d55fcc96222f970d82029a2e3f927
daylight-x64 <theodor7531@gmail.com>
2026-06-25 19:31:36 +0200

    added placing of custom blocks

--- src/main/java/net/daylight/backroomsPluginForPaper/Custom_blocks/CustomBlockType.java
+++ src/main/java/net/daylight/backroomsPluginForPaper/Custom_blocks/CustomBlockType.java
@@ -2,20 +2,28 @@ package net.daylight.backroomsPluginForPaper.Custom_blocks;
 
 import org.bukkit.Instrument;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Note;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.NoteBlock;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public enum CustomBlockType {
-    BOTTOM_WALL(Instrument.DIDGERIDOO, new Note(0)),
-    MIDDLE_WALL(Instrument.DIDGERIDOO, new Note(1));
+    BOTTOM_WALL(Instrument.DIDGERIDOO, new Note(0), "Wall bottom"),
+    MIDDLE_WALL(Instrument.DIDGERIDOO, new Note(1), "Wall middle"),;
 
     private final Instrument instrument;
     private final Note note;
+    private final String displayName;
 
-    CustomBlockType(Instrument instrument, Note note) {
+    CustomBlockType(Instrument instrument, Note note, String displayName) {
         this.instrument = instrument;
         this.note = note;
+        this.displayName = displayName;
+
     }
 
     public static boolean isCustomBlockType(Block block) {
@@ -29,6 +37,22 @@ public enum CustomBlockType {
         }
 
         return false;
+    }
+
+    public ItemStack createItem(JavaPlugin plugin) {
+        ItemStack item = new ItemStack(Material.NOTE_BLOCK);
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName("§e" + this.displayName); // Yellow name
+
+            // Tag it with a persistent key so we know exactly which custom block this is
+            NamespacedKey key = new NamespacedKey(plugin, "custom_block_type");
+            meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, this.name());
+
+            item.setItemMeta(meta);
+        }
+        return item;
     }
 
     public Instrument getInstrument() {
